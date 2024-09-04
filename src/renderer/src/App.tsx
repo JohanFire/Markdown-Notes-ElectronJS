73b4935219c29c96b2d78e52c728a905d17d948a
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2024-09-03 22:26:49 -0600

    ActionButtonsRow component, includes NewNote and DeleteNote buttons

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
+import { ActionButtonsRow, Content, DraggableTopBar, RootLayout, Sidebar } from '@/components'
 
 const App = () => {
   return (
@@ -6,7 +6,9 @@ const App = () => {
       <DraggableTopBar />
       <RootLayout>
         {/* <Sidebar className="p-2 border-4 border-red-500 text-blue-500">Sidebar</Sidebar> */}
-        <Sidebar className="p-2">Sidebar</Sidebar>
+        <Sidebar className="p-2 bg-gray-600/50">
+          <ActionButtonsRow className="flex justify-between mt-1" />
+        </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
     </>
---
+++ src/renderer/src/components/Button/index.ts
@@ -0,0 +1,4 @@
+export * from './ActionButton'
+export * from './ActionsButtonsRow'
+export * from './DeleteButton'
+export * from './NewNoteButton'
