2fc7047f0d4359b49197b214cb9eb677e9605aea
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2024-09-03 21:53:55 -0600

    renderer App.tsx

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,16 +3,11 @@ import { Content, RootLayout, Sidebar } from '@/components'
 function App() {
   return (
     <RootLayout>
-      <Sidebar className="p-2 border-4 border-red-500 text-blue-500">Sidebar</Sidebar>
-      <Content className="p-2 border-4 border-blue-500 text-blue-500">Content</Content>
+      {/* <Sidebar className="p-2 border-4 border-red-500 text-blue-500">Sidebar</Sidebar> */}
+      <Sidebar className="p-2">Sidebar</Sidebar>
+      <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
     </RootLayout>
   )
 }
 
 export default App
-
-{
-  /* <div className="flex h-full items-center justify-center">
-<span className="text-4xl text-blue-500">Hello from Electron</span>
-</div> */
-}
